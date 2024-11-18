8d3b8eab1cdc069572e532957be5bd577699f352
Zach <zach@coinculture.info>
2024-11-18 12:03:34 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    LACONIC_API_URL: 'https://laconicd.laconic.com/api',
+    LACONIC_API_URL: 'https://laconicd-sapo.laconic.com/api',
   },
 };
 
